39e762b472fcce77b78c0b366dfb7ea002c33f6f
Petra <petra@behemot.biz>
2024-09-17 21:42:42 +0200

    Migrate product model

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Ingredient, Item
 
 # Register your models here.
+@admin.register(Item)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'get_ingredient_names') #maybe remove id from list display
+    list_display_links = ('name',)
+    filter_horizontal = ('ingredients',)
+
+    #return list of ingredient names, separated with commas
+    def get_ingredient_names(self, obj):
+        return ", ".join([ingredient.name for ingredient in obj.ingredients.all()])
+    
+    get_ingredient_names.short_description = 'Ingredients'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
