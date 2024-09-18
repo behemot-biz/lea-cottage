9aee30e7d909f79869f43d210f08405a12ab5223
Petra <petra@behemot.biz>
2024-09-18 15:12:29 +0200

    feat: Add views and template to display product stock items. Update urls to display stock item list

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ItemList.as_view(), name='home'),
+    path('stock_item', views.StockItemList.as_view(), name='stock_item_list'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, StockItem
 
 
 # Create your views here.
@@ -8,3 +8,25 @@ class ItemList(generic.ListView):
     queryset = Item.objects.all().order_by("name")
     template_name = "product/item_list.html"
     context_object_name = "items"
+
+
+class StockItemList(generic.ListView):
+    model = StockItem
+    template_name = "product/stock_item_list.html"
+    context_object_name = "stock_items"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Add stock items and their associated ingredients to the context
+        stock_items_with_ingredients = []
+        for stock_item in context['stock_items']:
+            # Fetch related ingredients from the linked Item
+            ingredients = stock_item.item.ingredients.all()
+            stock_items_with_ingredients.append({
+                'stock_item': stock_item,
+                'ingredients': ingredients,
+            })
+        
+        context['stock_items_with_ingredients'] = stock_items_with_ingredients
+        return context
