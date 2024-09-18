45e90493d2ec60da1c5f03cce0e0c8677b528a8b
Petra <petra@behemot.biz>
2024-09-18 20:28:04 +0200

    feat: Install django-summernote, add filter and search to StockItemAdmin

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Ingredient, Item, Quantity, Preserve, StockItem
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 @admin.register(Item)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
     list_display = ('id', 'name', 'get_ingredient_names') #maybe remove id from list display
     list_display_links = ('name',)
     filter_horizontal = ('ingredients',)
@@ -16,21 +17,24 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 @admin.register(Ingredient)
-class IngredientAdmin(admin.ModelAdmin):
+class IngredientAdmin(SummernoteModelAdmin):
     list_display = ('id', 'name')
 
 
 @admin.register(Quantity)
-class QuantityAdmin(admin.ModelAdmin):
+class QuantityAdmin(SummernoteModelAdmin):
     list_display = ('id', 'unit')
 
 
 @admin.register(Preserve)
-class PreserveAdmin(admin.ModelAdmin):
+class PreserveAdmin(SummernoteModelAdmin):
     list_display = ('id', 'method')
 
 
 @admin.register(StockItem)
-class StockItemAdmin(admin.ModelAdmin):
+class StockItemAdmin(SummernoteModelAdmin):
     list_display = ('id', 'item', 'item_quantity', 'quantity', 'preserve', 'created_on', 'status')
     list_display_links = ('item',)
+    list_filter = ('status', 'item', 'created_on')
+    search_fields = ['item']
+    #summernote_fields = ('item',)
